1d655118afb900c892fd4ce11f33a13c4916f99b
Irfan1286 <115062206+Irfan1286@users.noreply.github.com>
2022-11-07 16:24:11 +0600

    .

--- Exercises/healthy programmer/Healthy programmer ex - 7 day 13.py
+++ Exercises/healthy programmer/Healthy programmer ex - 7 day 13.py
@@ -7,29 +7,51 @@
 # Rules
 # Pygame module to play audio
 
-import pygame
+from pygame import mixer
 import time
 
-t = time.strftime('%H: %M: %S')
+timing = time.strftime('%H: %M: %S', time.localtime())
 start = '09: 00: 00'
 end = '17: 00: 00'
 
-pygame.mixer.init()
+minute = time.strftime('%M', time.localtime())
+
+def music(file, requiredinp):   # plays music
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play(50)    # The int in () is number of times to loop
+    while True:
+        user = input()
+        if user == requiredinp:
+            mixer.music.stop()
+            break
 
 
-if t >= start and t <= end:
-    count = 0
-    while count < 1:
-        user = 8
-        while user != 'drank':
-            time.sleep(5)
-            pygame.mixer.music.load('water.mp3')
-            pygame.mixer.music.set_volume(1.0)
-            pygame.mixer.music.play()
+def log(file_name, message):    # Writes to file
+    with open(file_name, 'a') as file:
+        file.writelines(f'[{timing}] {message}\n')
+    print(message)
 
 
+if __name__ == '__main__':
+    eyes_time = time.time()     # this initializes starting time
+    water_start = time.time()
+    exercise_start = time.time()
+    while timing > start and timing < end:
+        if time.time() - water_start > 20:    # It's used to find how much time has passed and
+            print('Type "Done" and enter to stop alarm')
+            water_start = time.time()  # THis resets the init_time again
+            music('water.mp3', 'Done')
+            log('Health.txt', 'DRANK WATER')
 
+        if time.time() - exercise_start > 2700:    # the integer is the difference
+            print('Type "Done" and enter to stop alarm')
+            exercise_start = time.time()
+            music('Exercise.mp3', 'Done')
+            log('Health.txt', 'You have done the Exercise')
 
-
-
-
+        if time.time() - eyes_time > 1800:    # the integer is the difference
+            print('Type "Done" and enter to stop alarm')
+            eyes_time = time.time()
+            music('eyes.mp3', 'Done')
+            log('Health.txt', 'Eyes Exercise Performed')
--- week 4/Coroutines day 22.py
+++ week 4/Coroutines day 22.py
@@ -1,1 +1,30 @@
+import time
 
+# Coroutines are used to initialize the main function which may take a long time and then goes to loop
+
+def searcher():
+    print('Function will start in 4 seconds')
+    time.sleep(4)   # Can be any sort of time taking task
+    book = 'Geronimo, Horse, animals, Dinosaur, Schools are fools'
+
+    while True:
+        Text = (yield)  # THis is now a coroutine ALONG WITH WHILE TRUE where its become a generator
+        if Text in book:
+            time.sleep(1)
+            print('YES The book is available in library')
+        else:
+            time.sleep(1)
+            print('Sorry The book you are searching for is not available')
+
+
+search = searcher()
+next(search)        # return the items to an iterator and the actual functions is executed once and
+count = 0           # The remaining time it will go to While True loop
+
+
+while count<10:
+    user = input('Please enter what you want to find!\n')
+    search.send(user)
+    count+=1
+
+search.close()  # Will close the coroutine
